6a06f6cb6f96a1cdd9bd27d5f9f4549a665debb0
Yasser-Mohamed <yassermr418@gmail.com>
2022-12-22 02:20:18 +0200

    second modifiy

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@
 */
 const allSections = document.querySelectorAll("section");
 const navList = document.getElementById("navbar__list");
-
+const frag = document.createDocumentFragment()
 
 /**
  * End Global Variables
@@ -41,51 +41,59 @@ const navList = document.getElementById("navbar__list");
 */
 
 // build the nav
-const navBuilder = ()=>{
-    let navUI = "";
-    allSections.forEach(section =>{
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
-        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
-    })
-    navList.innerHTML= navUI;
-}
-navBuilder();
 
-// Add class 'active' to section when near top of viewport
-const offset = (section)=>{
-    return Math.floor(section.getBoundingClientRect().top);
-}
-const removeActive = (section) =>{
-    section.classList.remove("your-active-class");
-};
-const addActive = (condition, section)=>{
-    if(condition){
-    section.classList.add("your-active-class");
-    }
-};
-const sectionActivation =()=>{
-    allSections.forEach(section =>{
-        const elementOffSet =offset(section);
-        inViewPort =() => elementOffSet <150 && elementOffSet >= -150;
-        removeActive(section);
-        addActive(inViewPort(),section);
-    });
-};
-window.addEventListener("scroll",sectionActivation);
-// Scroll to anchor ID using scrollTO event
-const scrolling =()=>{
-    const links = document.querySelectorAll("navbar__list a");
-    links.forEach(link=>{
-        link.addEventListener("click",()=>{
-            for(i = 0 ; i< allSections ; i++){
-                allSections[i].addEventListener("click",sectionScroll(link));
 
-            }
+    allSections.forEach(section =>{
+        var secID = section.getAttribute("id");
+        var secData =section.getAttribute("data-nav");
+        const liTem = document.createElement("li")
+        const a = document.createElement("a");
+        a.classList.add("menu__link");
+        a.href = `#${secID}`;
+        a.textContent = secData;
+        a.addEventListener("click" , ev=>{
+            ev.preventDefault();
+            section.scrollIntoView({
+                behavior : "smooth"
+            });
         });
-    });
-};
-scrolling();
+        liTem.appendChild(a);
+        frag.appendChild(liTem);
+    })
+    navList.appendChild(frag);
+    
+// Add class 'active' to section when near top of viewport
+window.addEventListener("scroll", scrolling);
+const links_item = document.querySelectorAll("a.menu__link") 
+   function scrolling(){
+    allSections.forEach(section=>{
+        const secHighy = section.getBoundingClientRect().top;
+        const secTit =section.getAttribute("data-nav");
+        if(secHighy > 0 && secHighy < 250) {
+            section.classList.add("your-active-class");
+            links_item.forEach(link=>{
+                if(link.textContent === secTit) {
+                    link.classList.add("activation")
+                }
+                else {
+                    link.classList.remove("activation")
+                }
+            })
+        }
+        else {
+            section.classList.remove("your-active-class");
+        }
+       })
+   }
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+
+
+
 
 /**
  * End Main Functions
